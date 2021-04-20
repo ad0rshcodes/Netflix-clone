af49a8a3fd7a91dc5abbf121bc28afcd6ba5accf
Adarsh Sharma <adarshsharma6218@gmail.com>
2021-04-20 18:19:48 +0530

    fix category names

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
       <Row isLargeRow title="Trending now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comdy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="BRomance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Google Docs © umentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
