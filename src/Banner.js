b94265354472c60c0de0be97899e1458413b6110
Avani Sood <avanisood0806@gmail.com>
2021-04-21 23:54:08 +0530

    fix play button

--- src/Banner.js
+++ src/Banner.js
@@ -2,9 +2,40 @@ import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import requests from "./requests";
 import "./Banner.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Banner() {
+
+function Banner({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+    }
+    fetchData();
+  }, [fetchUrl]);
+  const youtubeOpts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const movieClicked = (moviename) => {
+    console.log(moviename);
+    if (trailerUrl !== "") setTrailerUrl("");
+    else {
+      movieTrailer(moviename)
+        .then((url) => {
+          const urlParamV = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParamV.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   const [movie, setMovie] = useState({});
   useEffect(() => {
     async function fetchMovie() {
@@ -18,10 +49,10 @@ function Banner() {
     fetchMovie();
   }, []);
   console.log(movie);
-    const [webkit, setWebkit] = useState(false);
+  const [webkit, setWebkit] = useState(false);
 
-    const descWebkitHandler = () => {
-    setWebkit(!webkit)
+   const descWebkitHandler = () => {
+   setWebkit(!webkit)
     }
   return (
     <header
@@ -38,13 +69,16 @@ function Banner() {
           {movie?.name || movie?.title || movie?.orginal_name}
         </h1>
         <div className="banner__buttons">
-          <button className="banner__button">Play</button>
-          
+          <button className="banner__button" onClick={() =>
+              movieClicked(movie.name || movie.title || movie.orginal_name)
+            }>Play</button>
           <button className="banner__button" onClick={descWebkitHandler}>More Info</button>
         </div>
         <p className={`banner__description ${webkit && 'webkit_disable'}`}>{movie?.overview}</p>
       </div>
-      <div className="banner--fadeBottom" />
+      <div className="banner--fadeBottom">
+      {trailerUrl !== "" && <YouTube videoId={trailerUrl} opts={youtubeOpts} />}
+      </div>
     </header>
   );
 }
